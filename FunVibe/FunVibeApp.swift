a1aeb429efab04a61363a9f4b55439bd748f5817
A Sathiesh <satwebfr@gmail.com>
2025-12-10 10:17:33 +0100

    filenames chanaged

---
+++ FunVibe/FunVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunVibeAppApp.swift
+//  FunVibeApp
+//
+//  Created by Apprenant 84 on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FunVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
