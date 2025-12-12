06b902a1a491362ab3291b2454140d406e356a8e
A Sathiesh <satwebfr@gmail.com>
2025-12-12 10:39:34 +0100

    Static Login validation

--- FunVibe/ProfileView.swift
+++ FunVibe/ProfileView.swift
@@ -9,21 +9,46 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        ZStack {
-            //Backgroound
-            Image("Background")
-                .resizable()
-                .opacity(0.5)
+        NavigationStack {
+            ZStack {
+                //Backgroound
+                Image("Background")
+                    .resizable()
+                    .opacity(0.5)
 
-            //Content
-            VStack {
-                Text("Profile")
-                    .font(.largeTitle)
-                    .foregroundColor(.primary)
+                //Content
+                if UserDefaults.standard.bool(forKey: "isLoggedIn") == false {
+                    NavigationLink(destination: UserLoginView()){
+                        Text("Please login to access this page")
+                    }
+                } else {
+                    VStack {
+                        Text("Profile")
+                            .font(.largeTitle)
+                            .foregroundColor(.primary)
+
+                            .padding(20)
+
+                        NavigationLink(destination: ExploreView()) {
+                            Image(systemName: "person.slash")
+                                .font(Font.title.bold()).foregroundColor(.orange)
+                        }.simultaneousGesture(TapGesture().onEnded {
+                            setLogout()
+                        })
+                    }
+                }
+
+
             }
+            .ignoresSafeArea(edges: .all)
+            .navigationBarTitle("Profile")
 
         }
-        .ignoresSafeArea(edges: .all)
+
+    }
+
+    func setLogout() -> Void {
+        UserDefaults.standard.set(false, forKey: "isLoggedIn")
     }
 }
 
